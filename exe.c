2e77dcb8c6453f31e6f802a9f541efed044f13a2
vanemcb <vanem2201@gmail.com>
2021-04-05 19:01:44 -0500

    Project updated

--- exe.c
+++ exe.c
@@ -10,8 +10,10 @@ void exe(char **argv)
 	if (return_fork == 0)
 	{
 		if (execve(argv[0], argv, NULL) == -1)
+		{
 			perror("Error:");
-		exit (0);
+			exit(0);
+		}
 	}
 	wait(&status);
 }
--- shell.c
+++ shell.c
@@ -3,35 +3,36 @@
 int main(void)
 {
 	size_t size = 0;
-	int line_read, re_strcmp = 1;
+	int line_read = 0, re_strcmp = 1;
 	char *buffer = NULL, **arr_str = NULL;
 
-	while (1)
+	while (line_read != EOF)
 	{
 		printf("#cisfun$ ");
 		line_read = getline(&buffer, &size, stdin);
 
-		if (line_read == -1)
-			printf("Error");
-
-		buffer[line_read - 1] = '\0';
-		re_strcmp = strcmp(buffer, "exit");
-
-		if (re_strcmp == 0)
-			break;
-
-		/*write(STDOUT_FILENO, buffer, line_read);*/
-		arr_str = _token(buffer);
-		exe(arr_str);
-
-		/*while (arr_str[i])
+		if (line_read != -1)
 		{
-			printf("%s\n", arr_str[i]);
-			i++;
+			buffer[line_read - 1] = '\0';
+			re_strcmp = strcmp(buffer, "exit");
+
+			if (re_strcmp == 0)
+				break;
+
+			/*write(STDOUT_FILENO, buffer, line_read);*/
+			arr_str = _token(buffer);
+			exe(arr_str);
+
+			/*while (arr_str[i])
+			{
+				printf("%s\n", arr_str[i]);
+				i++;
+			}
+			i = 0;*/
+			free(arr_str);
 		}
-		i = 0;*/
 	}
-	free(arr_str);
+
 	free(buffer);
 	return 0;
 }
