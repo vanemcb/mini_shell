be5f0a9e994228c9bad94a2907d988de198aa182
vanemcb <vanem2201@gmail.com>
2021-04-08 12:18:26 -0500

    Mini shell without leaks

--- exe.c
+++ exe.c
@@ -4,7 +4,7 @@
 void exe(char **argv)
 {
 	pid_t return_fork;
-	int status, return_stat;
+	int status, return_stat, f = 0;
 	char **split_path = NULL, *path = NULL;
 	struct stat st;
 
@@ -15,7 +15,11 @@ void exe(char **argv)
 		argv[0] = _concpath(split_path, argv[0]);
 		if (argv[0][0] == 'n')
 			return_stat = -1;
-		return_stat = 0;
+		else
+		{
+			return_stat = 0;
+			f = 1;
+		}
 		free(split_path);
 		free(path);
 	}
@@ -34,7 +38,9 @@ void exe(char **argv)
 			}
 		}
 	}
-
 	wait(&status);
-	/*free(argv[0]);*/
+
+	/* Free para liberar argv[0] si se entra al primer if */
+	if (f == 1)
+		free(argv[0]);
 }
